a68b2bb7e55de1a130a256ff16f45fbc858ad66b
sebas411 <sebasma411@gmail.com>
2021-08-04 23:11:25 -0600

    draw triangle functionality added (flawed)

--- python/gl.py
+++ python/gl.py
@@ -1,5 +1,9 @@
 import struct
+from collections import namedtuple
 from obj import Obj
+
+V2 = namedtuple('vertex2d', ['x', 'y'])
+V3 = namedtuple('vertex3d', ['x', 'y', 'z'])
 
 r = None
 
@@ -100,11 +104,7 @@ class Renderer(object):
     windowY = int(y01 * self.vph + self.vpy)
     self.point(windowX, windowY)
   
-  def line(self, nx0, ny0, nx1, ny1):
-    x0 = int((nx0+1)/2 * self.vpw + self.vpx)
-    x1 = int((nx1+1)/2 * self.vpw + self.vpx)
-    y0 = int((ny0+1)/2 * self.vph + self.vpy)
-    y1 = int((ny1+1)/2 * self.vph + self.vpy)
+  def deNormalizedLine(self, x0, y0, x1, y1):
     dy = abs(y1 - y0)
     dx = abs(x1 - x0)
     steep = dy > dx
@@ -119,7 +119,7 @@ class Renderer(object):
     threshold =  dx
     y = y0
 
-    for x in range(x0, x1):
+    for x in range(x0, x1, 1 if x1 > x0 else -1):
 
       if steep:
         self.point(y, x)
@@ -129,16 +129,80 @@ class Renderer(object):
       offset += 2 * dy
 
       if offset >= threshold:
-        y += 1 if y0 < y1 else -1
+        if y0 < y1: y += 1
+        else: y -= 1
         threshold += 2 * dx
+  
+  def vertexLine(self, A, B):
+    x0 = A.x
+    x1 = B.x
+    y0 = A.y
+    y1 = B.y
+    self.deNormalizedLine(x0, y0, x1, y1)
+
+  def line(self, nx0, ny0, nx1, ny1):
+    x0 = int((nx0+1)/2 * self.vpw + self.vpx)
+    x1 = int((nx1+1)/2 * self.vpw + self.vpx)
+    y0 = int((ny0+1)/2 * self.vph + self.vpy)
+    y1 = int((ny1+1)/2 * self.vph + self.vpy)
+    self.deNormalizedLine(x0, x1, y0, y1)
+
+  def triangleWireframe(self, A, B, C):
+    self.vertexLine(A, B)
+    self.vertexLine(B, C)
+    self.vertexLine(C, A)
+
+  def triangle(self, A, B, C):
+    if A.y > B.y:
+      A, B = B, A
+
+    if A.y > C.y:
+      A, C = C, A
+
+    if B.y > C.y:
+      B, C = C, B
+
+    dx_ac = C.x - A.x
+    dy_ac = C.y - A.y
+    mi_ac = dx_ac/dy_ac
+
+    dx_ab = B.x - A.x
+    dy_ab = B.y - A.y
+    
+    mi_ab = dx_ab/dy_ab
+
+    for y in range(A.y, B.y +1):
+      xi = round(A.x - mi_ac * (A.y - y))
+      xf = round(A.x - mi_ab * (A.y - y))
+
+      if xi > xf:
+        xi, xf = xf, xi
+
+      for x in range(xi, xf + 1):
+        self.point(x, y)
+
+    dx_bc = C.x - B.x
+    dy_bc = C.y - B.y
+    mi_bc = dx_bc/dy_bc
+
+    for y in range(B.y, C.y + 1):
+      xi = round(A.x - mi_ac * (A.y - y))
+      xf = round(B.x - mi_bc * (B.y - y))
+
+      if xi > xf:
+        xi, xf = xf, xi
+
+      for x in range(xi, xf + 1):
+        self.point(x, y)
 
   def load(self, filename, translate, scale):
     model = Obj(filename)
 
     for face in model.faces:
-      for j in range(len(face)):
+      vertexNum = len(face)
+      for j in range(vertexNum):
         f1 = face[j][0]
-        f2 = face[(j+1)%len(face)][0]
+        f2 = face[(j+1) % vertexNum][0]
 
         v1 = model.vertices[f1 - 1]
         v2 = model.vertices[f2 - 1]
@@ -231,8 +295,10 @@ def glFinish():
 
 
 glInit()
-glCreateWindow(1000, 1000)
-glViewPort(0, 0, 1000, 1000)
-glLoad('./models/indoor plant_02.obj', (0, -3), (0.25, 0.25))
+glCreateWindow(200, 200)
+glViewPort(0, 0, 200, 200)
+r.triangle(V2(10, 70),  V2(50, 160), V2(70, 80))
+r.triangle(V2(180, 50), V2(150, 1),  V2(70, 180))
+r.triangle(V2(180, 150), V2(120, 160), V2(130, 180))
 
 glFinish()
